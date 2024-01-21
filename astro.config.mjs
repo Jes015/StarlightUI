c979e57a5847f30a910be8dbf480a9d2a10c90bc
Jes015 <jes.18.sin.15@gmail.com>
2024-01-21 14:18:33 -0500

    feat: add components section (#1)
    close #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,20 +11,30 @@ export default defineConfig({
       social: {
         github: 'https://github.com/withastro/starlight'
       },
-      sidebar: [{
-        label: 'Guides',
-        items: [
-          // Each item here is one entry in the navigation menu.
-          {
-            label: 'Example Guide',
-            link: '/guides/example/'
-          }]
-      }, {
-        label: 'Reference',
-        autogenerate: {
-          directory: 'reference'
-        }
-      }],
+      sidebar: [
+        {
+          label: 'Components',
+          autogenerate: {
+            directory: 'components'
+          }
+        },
+        {
+          label: 'Guides',
+          items:
+            [
+              // Each item here is one entry in the navigation menu.
+              {
+                label: 'Example Guide',
+                link: '/guides/example/'
+              }
+            ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference'
+          }
+        }],
       customCss: [
         './src/tailwind.css',
       ],
